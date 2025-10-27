d626f235848a21bd8792f97501506daf65facce7
QuantumCoder325 <trevor.choi.tech@outlook.com>
2025-10-27 04:00:08 -0400

    welcome screen changes based on account status

--- OTAMS_project/app/src/main/java/com/example/otams_project/LoggedInActivity.java
+++ OTAMS_project/app/src/main/java/com/example/otams_project/LoggedInActivity.java
@@ -6,28 +6,67 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoggedInActivity extends AppCompatActivity {
+
+    Class<?> logoutButtonDestination = MainActivity.class;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
         Account account = (Account) getIntent().getSerializableExtra("ACCOUNT");
-        String roleToDisplay;
-        if(account == null){
-            roleToDisplay ="User";
-        } else if (account.getRole() == null){
-            roleToDisplay ="User";
-        } else {
-            roleToDisplay = account.getRole();
+
+        String roleToDisplay = "User";
+        String welcomeMessage = "Account should not be null";
+        String logoutButtonMessage = "Logout";
+        String toastMessage = "Uh oh, something has gone wrong!";
+
+        if (account != null) {
+            if (!account.getRole().equals("admin")) {
+                Button adminControlsButton = findViewById(R.id.adminButton);
+                adminControlsButton.setVisibility(View.INVISIBLE);
+            }
+
+            if (account.getRole() != null) {
+                roleToDisplay = account.getRole();
+            }
+
+
+            switch (account.getStatus()) {
+                case "approved":
+                    welcomeMessage = "Welcome! Successfully Logged in as " + roleToDisplay;
+                    toastMessage = account.getEmail()+ " has signed in, they are a(n) " + account.getRole();
+                    break;
+                case "pending":
+                    welcomeMessage = "Your registration for the " + roleToDisplay + " role is pending approval";
+                    logoutButtonMessage = "Back to login";
+                    logoutButtonDestination = LoginActivity.class;
+                    toastMessage = "Hang in there " + account.getUser().getFirstName() + "!";
+                    break;
+                case "rejected":
+                    welcomeMessage = "Your registration for the " + roleToDisplay + " role has been rejected, please contact 123-456-7890 for help";
+                    logoutButtonMessage = "Return";
+                    toastMessage = account.getEmail()+ " has attempted to sign in, they are a(n) " + account.getRole();
+                    break;
+                default:
+                    welcomeMessage = "Registration status is an unexpected value";
+                    break;
+            }
+
+
         }
 
         TextView welcomeText=findViewById(R.id.textView3);
-        welcomeText.setText("Welcome! Successfully Logged in as " + roleToDisplay);
+        welcomeText.setText(welcomeMessage);
 
+        Button logoutButton = findViewById(R.id.logoutInput);
+        logoutButton.setText(logoutButtonMessage);
 
+        Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show();
 
 
     }
@@ -36,13 +75,13 @@ public class LoggedInActivity extends AppCompatActivity {
         if (account != null) {
             account.logout();
         }
-        startActivity( new Intent(LoggedInActivity.this , MainActivity.class));
+        startActivity( new Intent(LoggedInActivity.this , logoutButtonDestination));
         finish();
     }
     public void onAdminButtonClick(View view){
         Account account = (Account) getIntent().getSerializableExtra("ACCOUNT");
         assert account != null;
-        if(account.getEmail().compareTo("admin.root@gmail.com") == 0){
+        if(account.getRole().equals("admin")){
             startActivity(new Intent(LoggedInActivity.this, AdminActivity.class));
         }
 
--- OTAMS_project/app/src/main/java/com/example/otams_project/LoginActivity.java
+++ OTAMS_project/app/src/main/java/com/example/otams_project/LoginActivity.java
@@ -36,7 +36,6 @@ public class LoginActivity extends AppCompatActivity implements Login {
     }
 
     public void approveSignIn(Account account) {
-        Toast.makeText(this, account.getEmail()+ " has signed in, they are a " + account.getRole(), Toast.LENGTH_LONG).show();
         account.login();
         Intent intent=new Intent(LoginActivity.this,LoggedInActivity.class);
         intent.putExtra("ACCOUNT", account);
